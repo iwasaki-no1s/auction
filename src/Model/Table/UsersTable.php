9bba82d92a9ef351dd4ea37d79e84e5aeb85bb35
takumi-no1s <hoya.takumi@no1s.biz>
2018-06-18 15:11:20 +0900

    usertable commit

---
+++ src/Model/Table/UsersTable.php
@@ -0,0 +1,59 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+class UsersTable extends Table
+{
+	public function initialize(array $config)
+	{
+		parent::initialize($config);
+		$this->table('users');
+		$this->displayField('user_name');
+		$this->primaryKey('id');
+		$this->addBehavior('Timestamp');
+		$this->hasMany('Products',[
+				'foreignKey'=>'user_id',
+				'joinType'=>'inner'
+		]);
+		$this->hasMany('Favorites',[
+				'foreignKey'=>'user_id',
+				'joinType'=>'inner'
+		]);
+		$this->hasMany('Bids',[
+				'foreignKey'=>'user_id',
+				'joinType'=>'inner',
+		]);
+	}
+
+	public function validationDefault(Validator $validator)
+	{
+		$validator
+		->integer('id')
+		->allowEmpty('id','create');
+		$validator
+		->email('email')
+		->requirePresence('email','create')
+		->notEmpty('email')
+		->add('email','unique',[
+				'rule'=>'validateUnique',
+				'provider'=>'table',
+				'message'=>"重複するemailです"]);
+		$validator
+		->requirePresence('password','create')
+		->notEmpty('password');
+		$validator
+		->requirePresence('user_name','create')
+		->notEmpty('user_name');
+		return $validator;
+	}
+
+	public function buildRules(RulesChecker $rules)
+	{
+		$rules->add($rules->isUnique(['email'],["message"=>"重複するemailです"]));
+		return $rules;
+	}
+}
