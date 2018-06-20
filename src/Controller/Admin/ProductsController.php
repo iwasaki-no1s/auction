65e5c889c065f0a086f68546410391e4fb97910b
takumi-no1s <hoya.takumi@no1s.biz>
2018-06-20 17:10:00 +0900

    index

--- src/Controller/Admin/ProductsController.php
+++ src/Controller/Admin/ProductsController.php
@@ -5,6 +5,14 @@ use App\Controller\Admin\AppController;
 
 class ProductsController extends AppController
 {	
+	public function index()
+	{
+		$products = $this->Products->find()
+		->contain(['Users','Categories','Bids'])
+		->where(['sold'=>0])
+		->all();
+		$this->set(compact('products'));
+	}
 	public function register()
 	{
 		$id = $this->MyAuth->user('id');
