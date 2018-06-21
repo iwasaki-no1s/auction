ade06f84dcd1ecbb3cb2785c28db588ce160b21b
takumi-no1s <hoya.takumi@no1s.biz>
2018-06-21 15:41:36 +0900

    bid check

--- src/Controller/Admin/BidsController.php
+++ src/Controller/Admin/BidsController.php
@@ -13,7 +13,19 @@ class BidsController extends AppController
 		$bid->user_id=$user_id;
 		$bid->product_id=$product_id;
 		$bid=$this->Bids->patchEntity($bid,$this->request->data);
-		//if($bid->price<);
+		$current = $this->Bids
+					->find()
+					->where(['product_id'=>$bid->product_id]);
+		$current_price = $current
+					->select(['max_price' => $current->func()->max('price')])
+					->first();
+		if($bid->price <= $current_price->max_price){     ;//if文の左辺が入力値でデータベースの値(右辺）と比べている
+			$this->Flash->error ( __ ( 'その金額では入札できません' ) );
+			return $this->redirect ( [
+					'controller' => 'products',
+					'action' => 'bid',$bid->product_id
+			]);
+		}
 		if($this->Bids->save($bid)){
 			$this->Flash->success(__('入札しました'));
 			return $this->redirect(['controller'=>'MyPages','action'=>'index']);
